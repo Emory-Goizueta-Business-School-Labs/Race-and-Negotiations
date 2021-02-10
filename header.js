73608ebca8f35142b736b6b9653a26b203aecb39
Brent Lineberry <brent.lineberry@emory.edu>
2021-02-10 13:29:20 -0500

    only fadein answers if chatHistory exists

--- header.js
+++ header.js
@@ -217,9 +217,14 @@ Qualtrics.SurveyEngine.addOnload(function()
 Qualtrics.SurveyEngine.addOnReady(function()
 {
   console.log('headerOnReady', {pmd, 'embeddedMessages':'${e://Field/messages}', context: this });
-  let choiceContainer = pmd.question.context.getChoiceContainer();
-  choiceContainer.className += " choices-fade";
-  pmd.messagesToChat(document.getElementById('chat-history'), pmd.messages);
+  
+  let chatHistory = document.getElementById('chat-history');
+
+  if (chatHistory) {
+    let choiceContainer = pmd.question.context.getChoiceContainer();
+    choiceContainer.className += " choices-fade";
+    pmd.messagesToChat(document.getElementById('chat-history'), pmd.messages);
+  }   
 });
 
 Qualtrics.SurveyEngine.addOnUnload(function() {
